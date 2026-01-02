10874550455b17644044659760adb9b37ece0937
Krystian Ryczek <kryczek89@gmail.com>
2026-01-02 22:52:02 +0100

    params:{ id: string[] } =>Promise<{ id: string[] }> cd.

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,11 +2,13 @@ import NavigationBar from "@/components/product-details/navigation-bar";
 import ProductDetails from "@/components/product-details/product-details";
 import { currentProduct } from "@/util/fetching-data";
 
-export default async function ProductDetailsPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+type Params = Promise<{ id: string }>;
+
+interface PageProps {
+  params: Params;
+}
+
+export default async function ProductDetailsPage({ params }: PageProps) {
   const curentParams = await params;
   const productId = Number(curentParams.id);
   const productData = await currentProduct(productId);
