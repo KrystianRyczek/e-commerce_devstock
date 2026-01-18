f3be6b8980fa7abeeea11449b40f5515cff525c6
Krystian Ryczek <kryczek89@gmail.com>
2026-01-18 16:08:06 +0100

    filter page update

--- app/products/@sidebar/default.tsx
+++ app/products/@sidebar/default.tsx
@@ -2,8 +2,8 @@ import SideBar from "@/components/side-bar/side-bar";
 import { brandsNameList, categoriesNameList } from "@/util/fetching-data";
 import { currencys } from "@/util/static-data";
 export default async function SideBarPage() {
-  const categories = await categoriesNameList;
-  const brands = await brandsNameList;
+  const categories = await categoriesNameList();
+  const brands = await brandsNameList();
   return (
     <SideBar
       currencys={currencys}
